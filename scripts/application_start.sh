79a2cac55e56588bd99e65d25c9d50a4000d8ea3
RanjitSinghChouhan <ranjeet.singh.chouhan94@gmail.com>
2023-07-11 18:56:56 +0530

    pm2 restart 0

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,4 +3,4 @@
 echo 'run application_start.sh: ' >> /home/ubuntu/final-cicd/deploy.log
 
 echo 'pm2 restart final-cicd' >> /home/ubuntu/final-cicd/deploy.log
-pm2 restart final-cicd >> /home/ubuntu/final-cicd/deploy.log
+pm2 restart 0 >> /home/ubuntu/final-cicd/deploy.log
